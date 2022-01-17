cc8b8d72746eaa14414b6c74696d193015058a1a
Michael Jones <michael.k.jones.20@gmail.com>
2022-01-16 19:12:38 -0600

    Completed codewars exercise 'Title-Case' refactored.

--- Title-Case.js
+++ Title-Case.js
@@ -4,15 +4,34 @@
 
 // Version 1
 //////////////////////////////
+// function titleCase(title, minorWords) {
+//     let minor = minorWords ? minorWords.toLowerCase().split(' ') : [];
+//     let result = title.split(' ');
+//
+//     result.map(function(element, index, array){
+//         if(index === 0) {
+//             array[index] =  element.charAt(0).toUpperCase() + element.toLowerCase().substr(1);
+//         }
+//         else {
+//             minor.includes(element.toLowerCase()) ?
+//                 array[index] = element.toLowerCase() :
+//                 array[index] = element.charAt(0).toUpperCase() + element.toLowerCase().substr(1);
+//         }
+//     });
+//
+//     return result.join(' ');
+// }
+
+// Version 2
+//////////////////////////////
 function titleCase(title, minorWords) {
     let minor = minorWords ? minorWords.toLowerCase().split(' ') : [];
     let result = title.split(' ');
 
-    result.map(function(element, index, array){
+    result.map((element, index, array) =>
         minor.includes(element.toLowerCase()) && index ?
             array[index] = element.toLowerCase() :
-            array[index] = element.charAt(0).toUpperCase() + element.toLowerCase().substr(1);
-    });
+            array[index] = element.charAt(0).toUpperCase() + element.toLowerCase().substr(1));
 
     return result.join(' ');
 }
