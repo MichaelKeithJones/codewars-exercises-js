f603a30fde483cc8d5d5e91526bcb759fd5a79bf
Michael Jones <michael.k.jones.20@gmail.com>
2022-01-21 15:12:05 -0600

    Completed codewar exercise Gaps-in-Primes.

--- Gap-in-Primes.js
+++ Gap-in-Primes.js
@@ -55,25 +55,57 @@
 
 // Version 3
 //////////////////////////////
-function isPrime(n, i)
-{
-    if (n <= 2)
-        return (n === 2);
-    if (n % i === 0)
-        return false;
-    if (i * i > n)
-        return true;
+// function isPrime(n, i)
+// {
+//     if (n <= 2)
+//         return (n === 2);
+//     if (n % i === 0)
+//         return false;
+//     if (i * i > n)
+//         return true;
+//
+//     return isPrime(n, i + 1);
+// }
+//
+// let gap = function(g, m, n) {
+//     let answer = [];
+//     let primes = [];
+//
+//     for(let j = m; j <= n; j++) {
+//         if (j%2 !== 0 && j%3 !== 0 && j%5 !== 0 && isPrime(j, 2)) { primes.push(j); }
+//     }
+//
+//     for(let index = 0; index < primes.length; index++){
+//         if (primes[index+1] - primes[index] === g) {
+//             answer.push(primes[index])
+//             answer.push(primes[index+1]);
+//             break;
+//         }
+//     }
+//     return answer.length === 0 ? null : answer;
+// }
 
-    return isPrime(n, i + 1);
+// Version 4 -- Fastest Solution
+//////////////////////////////
+function getPrimes(n) {
+    let primes = Array(n).fill(true);
+    for(let i = 2; i < Math.sqrt(n); i++){
+        if (primes[i] === true) {
+            let j = i * i;
+            while(j <= n) {
+                primes[j] = false;
+                j+=i;
+            }
+        }
+    }
+    return primes.map((element, index)=> { if (element === true) { return index; }}).filter(element=>element!==undefined && element !== 0 && element !== 1);
 }
 
 let gap = function(g, m, n) {
     let answer = [];
-    let primes = [];
 
-    for(let j = m; j <= n; j++) {
-        if (j%2 !== 0 && j%3 !== 0 && j%5 !== 0 && isPrime(j, 2)) { primes.push(j); }
-    }
+    let primes = getPrimes(n);
+    primes = primes.filter(element => element > m);
 
     for(let index = 0; index < primes.length; index++){
         if (primes[index+1] - primes[index] === g) {
@@ -84,5 +116,26 @@ let gap = function(g, m, n) {
     }
     return answer.length === 0 ? null : answer;
 }
+
+// Version 5
+//////////////////////////////
+// let isPrime = function(number) {
+//     for (let i = 2; i < number; i++) {
+//         if (number % i === 0) { return false; }
+//     }
+//     return true;
+// }
+//
+// let gap = function(g, m, n) {
+//     let mx = 0;
+//     for(m; m < n; m++) {
+//         if (isPrime(m)) {
+//             if ((m - mx) === g)
+//                 return [mx, m]
+//             mx = m;
+//         }
+//     }
+//     return null;
+// }
 
 console.log(gap(4, 100, 110));
