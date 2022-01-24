9d693f63f39daa678244ce826598764d939aa609
Michael Jones <michael.k.jones.20@gmail.com>
2022-01-24 09:26:03 -0600

    Completed codewar exercise Remove-First-and-Last-Character.

---
+++ Remove-First-and-Last-Character.js
@@ -0,0 +1,11 @@
+// Author: Michael K. Jones
+
+"use strict";
+
+// Version 1
+//////////////////////////////
+function removeChar(str){
+    return str.substr(1,str.length-2);
+}
+
+console.log(removeChar('eloquent'));
